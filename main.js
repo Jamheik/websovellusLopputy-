f9982eb33266034501bdb82f1d8aec62fdb34ed3
Jamheik <heikkinenjami03@gmail.com>
2024-10-16 13:08:34 +0300

    Improved ui design and fixed weather data fetching

--- main.js
+++ main.js
@@ -11,36 +11,38 @@ function throttle(func, limit) {
 }
 
 export function getWeather(city) {
-    console.log("asdasd");
-    fetch(`https://api.allorigins.win/get?url=${encodeURIComponent(`https://www.meteosource.com/api/v1/free/point?place_id=${city}&sections=current&timezone=auto&language=en&units=metric&key=kj76d80ni8zt4zjwayeib45l518nggtl2jdgpmkw`)}`)
+    console.log("Fetching weather data...");
+    fetch(`https://www.meteosource.com/api/v1/free/point?place_id=${city}&sections=current&timezone=auto&language=en&units=metric&key=kj76d80ni8zt4zjwayeib45l518nggtl2jdgpmkw`)
     .then(response => {
         if (!response.ok) {
             throw new Error('Network response was not ok');
         }
-        return response.json();
+        return response.text(); 
     })
     .then(data => {
         const tempText = document.getElementById('temp');
-        const parsedData = JSON.parse(data.contents);
+        const parsedData = JSON.parse(data);
         console.log(parsedData);
+
         if (!parsedData.current) {
             tempText.innerText = "city not found";
             throw new Error('City not found');
         }
+
         let temperature = parsedData.current.temperature;
         let summary = parsedData.current.summary;
-        if(temperature < 0) {
+
+        if (temperature < 0) {
             tempText.style.color = 'blue';
-        } else if(temperature > 15) {
+        } else if (temperature > 15) {
             tempText.style.color = 'red';
-        }
-        else {
+        } else {
             tempText.style.color = 'black';
         }
+
         tempText.innerText = `Current weather in ${city}: ${temperature}°C, ${summary}.`;
     })
     .catch(error => console.error('There has been a problem:', error));
-    
 }
 const throttledGetWeather = throttle(getWeather, 60000);
 window.getWeather = throttledGetWeather;
